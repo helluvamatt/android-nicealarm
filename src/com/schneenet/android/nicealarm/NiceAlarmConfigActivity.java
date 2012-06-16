877a9f509a3a649e212f4c70953b56d90facc58e
Matt Schneeberger <helluvamatt@gmail.com>
2012-06-16 09:52:28 -0500

    Skeleton app

---
+++ src/com/schneenet/android/nicealarm/NiceAlarmConfigActivity.java
@@ -0,0 +1,13 @@
+package com.schneenet.android.nicealarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NiceAlarmConfigActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
